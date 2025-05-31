b20d6b4d141e5c2439145176606d94f1ef585fc3
tfinity <publishappstore74@gmail.com>
2025-06-01 01:49:26 +0500

    Added increment button and variable state and attahced with view

--- androidApp/src/main/java/com/tfinity/counterapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/tfinity/counterapp/android/MainActivity.kt
@@ -7,11 +7,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
@@ -20,10 +25,14 @@ import com.tfinity.counterapp.Greeting
 
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                val counter= remember {
+                    mutableStateOf(0)
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
@@ -36,6 +45,14 @@ class MainActivity : ComponentActivity() {
                                 colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Blue)
                             )
                         },
+                        floatingActionButton = { FloatingActionButton(
+                            onClick = {
+                                counter.value++
+                            },
+                            shape = FloatingActionButtonDefaults.largeShape
+                        ) {
+                            Icon(Icons.Default.Add,"Add")
+                        } }
                     ) { innerPadding ->
                         Column(
                             modifier = Modifier
@@ -46,7 +63,7 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text("You have clicked the button this many times:")
                             Text(
-                                "4", style = MaterialTheme.typography.headlineMedium
+                                "${counter.value}", style = MaterialTheme.typography.headlineMedium
                             )
                         }
                     }
